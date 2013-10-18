cfdbe1f48653f6c4f1e49796c129e0947ea3ab92
Adriano Melo <adriano.melo@openbossa.org>
2013-10-18 19:41:38 -0300

    Android build files

---
+++ src/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libgestures
+LOCAL_CFLAGS    := -Wall -std=gnu++11
+LOCAL_LDFLAGS   := -llog -landroid
+LOCAL_SRC_FILES := \
+    gesture.cpp \
+    gesturemanager.cpp \
+    gesturerecognizer.cpp \
+    gestures/pan.cpp \
+    events/ainputeventhelper.cpp
+
+LOCAL_SHARED_LIBRARIES := libstlport
+
+LOCAL_C_INCLUDES := \
+    ndk/sources/cxx-stl/stlport/stlport/ \
+    hydralibs/webkit-nix/Source \
+    hydralibs/webkit-nix/include \
+    hydralibs/webkit-nix/include/WebKit2 \
+    hydralibs/webkit-nix/include/Platform \
+    hydralibs/webkit-nix/include/JavaScriptCore
+
+include $(BUILD_SHARED_LIBRARY)
