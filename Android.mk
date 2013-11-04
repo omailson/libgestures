dc52418ed85c8cd5d80211f7d6584dd992a55b3b
Mailson Menezes <mailson.menezes@openbossa.org>
2013-11-04 15:55:52 -0300

    Add conditional clause to build samples
    Build command: mm BUILD_LIBGESTURES_SAMPLES=1
    
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- Android.mk
+++ Android.mk
@@ -1,2 +1,8 @@
-LOCAL_PATH := $(call my-dir)
+LIBGESTURES_PATH := $(call my-dir)
+LOCAL_PATH := $(LIBGESTURES_PATH)
+
 include $(LOCAL_PATH)/src/Android.mk
+
+ifeq ($(BUILD_LIBGESTURES_SAMPLES), 1)
+include $(LIBGESTURES_PATH)/samples/Android.mk
+endif
---
+++ samples/Android.mk
@@ -0,0 +1,6 @@
+SAMPLES_PATH := $(call my-dir)
+
+LOCAL_PATH := $(SAMPLES_PATH)
+include $(CLEAR_VARS)
+
+include $(SAMPLES_PATH)/GesturesSampleAndroid/Android.mk
