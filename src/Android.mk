850ea25f245e71913bccee1a6512a930a3ec3925
Aloisio Almeida Jr <aloisio.almeida@openbossa.org>
2013-12-12 16:35:32 -0300

    Add libandroid as android build dependence
    It was explicit on LD_FLAGS but not included in LOCAL_SHARED_LIBRARIES

--- src/Android.mk
+++ src/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libgestures
 LOCAL_CFLAGS    := -Wall -std=gnu++11
-LOCAL_LDFLAGS   := -llog -landroid
+LOCAL_LDFLAGS   := -llog
 LOCAL_SRC_FILES := \
     gesture.cpp \
     gesturemanager.cpp \
@@ -20,7 +20,8 @@ LOCAL_SRC_FILES := \
     utils/vector2d.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libstlport
+    libstlport \
+    libandroid
 
 LOCAL_C_INCLUDES := \
     ndk/sources/cxx-stl/stlport/stlport/
